f8d0bed26f6bbc09c81a3f4e7e8d598aca1704ad
thanhngo-bit <98thanhngo@gmail.com>
2019-12-18 22:51:50 +0700

    Revert "Space shooting game with some features of networking"
    This reverts commit 1c4dc42abc9ab59ad42d786395e546544d999607.

--- SpaceGuardian/Form1.Designer.cs
+++ SpaceGuardian/Form1.Designer.cs
@@ -31,22 +31,12 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.MoveBgTimer = new System.Windows.Forms.Timer(this.components);
-            this.Player = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.LeftMoveTimer = new System.Windows.Forms.Timer(this.components);
             this.RightMoveTimer = new System.Windows.Forms.Timer(this.components);
             this.UpMoveTimer = new System.Windows.Forms.Timer(this.components);
             this.DownMoveTimer = new System.Windows.Forms.Timer(this.components);
-            this.MoveMunitionTimer = new System.Windows.Forms.Timer(this.components);
-            this.MoveEnemiesTimer = new System.Windows.Forms.Timer(this.components);
-            this.EnemiesMunitionTimer = new System.Windows.Forms.Timer(this.components);
-            this.ReplayBtn = new System.Windows.Forms.Button();
-            this.ExitBtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.labelScore = new System.Windows.Forms.Label();
-            this.labelLvl = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // MoveBgTimer
@@ -55,131 +45,31 @@
             this.MoveBgTimer.Interval = 15;
             this.MoveBgTimer.Tick += new System.EventHandler(this.MoveBgTimer_Tick);
             // 
-            // Player
+            // pictureBox1
             // 
-            this.Player.Image = ((System.Drawing.Image)(resources.GetObject("Player.Image")));
-            this.Player.Location = new System.Drawing.Point(317, 399);
-            this.Player.Name = "Player";
-            this.Player.Size = new System.Drawing.Size(50, 50);
-            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.Player.TabIndex = 0;
-            this.Player.TabStop = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(317, 399);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // LeftMoveTimer
             // 
             this.LeftMoveTimer.Interval = 5;
-            this.LeftMoveTimer.Tick += new System.EventHandler(this.LeftMoveTimer_Tick);
             // 
             // RightMoveTimer
             // 
             this.RightMoveTimer.Interval = 5;
-            this.RightMoveTimer.Tick += new System.EventHandler(this.RightMoveTimer_Tick);
             // 
             // UpMoveTimer
             // 
             this.UpMoveTimer.Interval = 5;
-            this.UpMoveTimer.Tick += new System.EventHandler(this.UpMoveTimer_Tick);
             // 
             // DownMoveTimer
             // 
             this.DownMoveTimer.Interval = 5;
-            this.DownMoveTimer.Tick += new System.EventHandler(this.DownMoveTimer_Tick);
-            // 
-            // MoveMunitionTimer
-            // 
-            this.MoveMunitionTimer.Enabled = true;
-            this.MoveMunitionTimer.Interval = 20;
-            this.MoveMunitionTimer.Tick += new System.EventHandler(this.MoveMunitionTimer_Tick);
-            // 
-            // MoveEnemiesTimer
-            // 
-            this.MoveEnemiesTimer.Enabled = true;
-            this.MoveEnemiesTimer.Tick += new System.EventHandler(this.MoveEnemiesTimer_Tick);
-            // 
-            // EnemiesMunitionTimer
-            // 
-            this.EnemiesMunitionTimer.Enabled = true;
-            this.EnemiesMunitionTimer.Interval = 20;
-            this.EnemiesMunitionTimer.Tick += new System.EventHandler(this.EnemiesMunitionTimer_Tick);
-            // 
-            // ReplayBtn
-            // 
-            this.ReplayBtn.Location = new System.Drawing.Point(218, 232);
-            this.ReplayBtn.Name = "ReplayBtn";
-            this.ReplayBtn.Size = new System.Drawing.Size(247, 41);
-            this.ReplayBtn.TabIndex = 1;
-            this.ReplayBtn.Text = "Restart";
-            this.ReplayBtn.UseVisualStyleBackColor = true;
-            this.ReplayBtn.Visible = false;
-            this.ReplayBtn.Click += new System.EventHandler(this.ReplayBtn_Click);
-            // 
-            // ExitBtn
-            // 
-            this.ExitBtn.Location = new System.Drawing.Point(218, 312);
-            this.ExitBtn.Name = "ExitBtn";
-            this.ExitBtn.Size = new System.Drawing.Size(247, 41);
-            this.ExitBtn.TabIndex = 2;
-            this.ExitBtn.Text = "Exit";
-            this.ExitBtn.UseVisualStyleBackColor = true;
-            this.ExitBtn.Visible = false;
-            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(279, 136);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 24);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "GAME OVER";
-            this.label1.Visible = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(13, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "SCORE:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(564, 13);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 24);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "LEVEL:";
-            // 
-            // labelScore
-            // 
-            this.labelScore.AutoSize = true;
-            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelScore.Location = new System.Drawing.Point(95, 13);
-            this.labelScore.Name = "labelScore";
-            this.labelScore.Size = new System.Drawing.Size(32, 24);
-            this.labelScore.TabIndex = 6;
-            this.labelScore.Text = "00";
-            // 
-            // labelLvl
-            // 
-            this.labelLvl.AutoSize = true;
-            this.labelLvl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLvl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelLvl.Location = new System.Drawing.Point(640, 13);
-            this.labelLvl.Name = "labelLvl";
-            this.labelLvl.Size = new System.Drawing.Size(32, 24);
-            this.labelLvl.TabIndex = 7;
-            this.labelLvl.Text = "00";
             // 
             // Form1
             // 
@@ -187,45 +77,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(684, 461);
-            this.Controls.Add(this.labelLvl);
-            this.Controls.Add(this.labelScore);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.ExitBtn);
-            this.Controls.Add(this.ReplayBtn);
-            this.Controls.Add(this.Player);
+            this.Controls.Add(this.pictureBox1);
             this.MaximumSize = new System.Drawing.Size(700, 500);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.BackColorChanged += new System.EventHandler(this.Form1_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer MoveBgTimer;
-        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer LeftMoveTimer;
         private System.Windows.Forms.Timer RightMoveTimer;
         private System.Windows.Forms.Timer UpMoveTimer;
         private System.Windows.Forms.Timer DownMoveTimer;
-        private System.Windows.Forms.Timer MoveMunitionTimer;
-        private System.Windows.Forms.Timer MoveEnemiesTimer;
-        private System.Windows.Forms.Timer EnemiesMunitionTimer;
-        private System.Windows.Forms.Button ReplayBtn;
-        private System.Windows.Forms.Button ExitBtn;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label labelScore;
-        private System.Windows.Forms.Label labelLvl;
     }
 }
 
